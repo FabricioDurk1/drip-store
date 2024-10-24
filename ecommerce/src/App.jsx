f0208ceb12c9f73ca0319cfb98219a61d00ed32e
Rafael Leite <costtaraphael@gmail.com>
2024-10-24 10:39:59 -0300

    f: criei o componente productList

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
-import ProductCard from "./components/ProductCard";
 import Section from "./components/Section";
 import "primeflex/primeflex.css";
+import productData from "./assets/productlist.json";
+import ProductList from './components/ProductList';
 
 const App = () => {
   return ( 
@@ -9,90 +10,7 @@ const App = () => {
       title={"Produtos em alta"}
       link={"https://redirect.link"}
     >
-      <div className="grid">
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold"><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            >
-              <div className="discount-card">
-                <h1 className="discount-text">30% OFF</h1>
-              </div>
-            </ProductCard></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            >
-              <div className="discount-card">
-                <h1 className="discount-text">30% OFF</h1>
-              </div>
-            </ProductCard></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-      </div>
-      <div className="grid">
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold"><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-        <div className="col">
-            <div className="text-center align-items-center p-0 border-round font-bold "><ProductCard
-              image={"./images/product.svg"}
-              description={"Tênis"}
-              name={"K-Swiss V8 - Masculino"}
-              price={"$200"}
-              priceDiscount={"$100"}
-            /></div>
-        </div>
-      </div>
+      <ProductList products={productData} />
     </Section>
    );
 }
