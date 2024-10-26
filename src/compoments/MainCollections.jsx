d420122b241c54480818d2d8023dc4e0c5736721
Fabricio Amorim <fabriciofkz61@gmail.com>
2024-10-26 02:02:30 -0300

    Adiciona botão de 'Comprar' e '30% OFF' nas imagens de Coleções em Destaque

--- src/compoments/MainCollections.jsx
+++ src/compoments/MainCollections.jsx
@@ -4,14 +4,28 @@ import './MainCollections.css';
 const MainCollections = () => {
   return (
     <div className="main-collections-container">
-    <h2 className="collections-title">Coleções em destaque</h2> {/* Adicionando o título */}
-    <div className="main-collections">
-      <img src="/collection-1.png" alt="Novo drop Supreme" />
-      <img src="/collection-2.png" alt="Coleção Adidas" />
-      <img src="/collection-3.png" alt="Novo Beats Bass" />
+      <h2 className="collections-title">Coleções em destaque</h2>
+      <div className="main-collections">
+        <div className="collection-item">
+          <img src="/collection-1.png" alt="Novo drop Supreme" />
+          <div className="discount-tag">30% OFF</div>
+          <button className="buy-button">Comprar</button>
+        </div>
+        <div className="collection-item">
+          <img src="/collection-2.png" alt="Coleção Adidas" />
+          <div className="discount-tag">30% OFF</div>
+          <button className="buy-button">Comprar</button>
+        </div>
+        <div className="collection-item">
+          <img src="/collection-3.png" alt="Novo Beats Bass" />
+          <div className="discount-tag">30% OFF</div>
+          <button className="buy-button">Comprar</button>
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
 
 export default MainCollections;
+
+
