41101c5b2483fe188a446529ffcf34b1d6967f77
Rafael Leite <costtaraphael@gmail.com>
2024-10-28 14:22:10 -0300

    ajuste no Gallery

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,14 +1,17 @@
 import Section from "./components/Section";
 import "primeflex/primeflex.css";
+import './App.css';
 import Cabecalho from './components/Cabecalho';
 import productData from "./assets/productlist.json";
+import imagesData from './assets/imageslist.json';
 import ProductList from './components/ProductList';
-import './App.css';
+import Gallery from "./components/Gallery";
 
 const App = () => {
   return ( 
     <>
       <Cabecalho/>
+      <Gallery imagesList={imagesData}/>
       <Section
         title={"Produtos em alta"}
         link={"https://redirect.link"}
--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Inclui o JS necessário para interações
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
