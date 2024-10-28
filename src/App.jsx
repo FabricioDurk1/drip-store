9ebf156af901d350470e19a5326e5960e452fb44
Rafael Leite <costtaraphael@gmail.com>
2024-10-28 19:36:37 -0300

    Adicionei o css dos componentes e bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import MainCollections from './components/MainCollections';
 import SecondaryCollections from './components/SecondaryCollections';
+import Cabecalho from './components/Cabecalho';
+import productData from "./assets/productlist.json";
+import imagesData from './assets/imageslist.json';
+import ProductList from './components/ProductList';
+import Gallery from "./components/Gallery";
+import Section from "./components/Section";
+import "primeflex/primeflex.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Cabecalho/>
+      <Gallery imagesList={imagesData}/>
       <MainCollections />
       <SecondaryCollections />
+      <Section
+        title={"Produtos em alta"}
+        link={"https://redirect.link"}
+      >
+        <ProductList products={productData} />
+      </Section>
     </div>
   );
 }
