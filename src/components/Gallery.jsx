0f4ed87ffed052f76c53ecf1d3fcb1099324dd40
Rafael Leite <costtaraphael@gmail.com>
2024-10-30 13:57:44 -0300

    import de images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,29 +4,26 @@ import './styles/gallery.css';
 const Gallery = ({imagesList, width, height}) => {
     return ( 
         <>
-        <Carousel interval={null}>
-            {imagesList.map((image, index)=>(
-                // <Carousel.Item  key={index}>
-                //      <div className="banner-carousel">
-                //         <p>Melhores ofertas personalizadas</p>
-                //         <h1>Queima de estoque</h1>
-                //     </div>
-                //     <img src={image.src} width={width} height={height} alt={`Slide ${index}`} />
-                // </Carousel.Item>
-                <Carousel.Item key={index}>
-                    <div className="d-flex align-items-center banner-carousel">
-                        <div className="text-content">
-                            <p>Melhores ofertas personalizadas</p>
-                            <h1>Queima de estoque</h1>
-                        </div>
+        <div className="d-flex align-items-center banner-carousel">
+            <div className="text-content">
+                <p className='title-text'>Melhores ofertas personalizadas</p>
+                <h1 className='promo-text'>Queima de estoque</h1>
+                <p className='description-text'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
+                <button className='see-offer'>Ver Ofertas</button>
+            </div>
+            <Carousel interval={null} fade>
+                {imagesList.map((image, index)=>(
+                    <Carousel.Item key={index}>
                         <div className="image-content">
+                            <div className="ornament">
+                                <img src="./images/ornament11.svg" alt="ornament" width="140px" height="140px"/>
+                            </div>
                             <img src={image.src} alt={`Slide ${index}`} width={width} height={height}/>
                         </div>
-                    </div>
-                </Carousel.Item>
-                
-            ))}
-        </Carousel>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </div>
         </> 
      );
 }
