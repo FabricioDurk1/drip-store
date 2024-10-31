4bdfea77f812f0c83f08838a9f995e2bfa3a5368
Rafael Leite <costtaraphael@gmail.com>
2024-10-30 21:48:39 -0300

    ajustes no menu collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,15 +32,15 @@ const Header = () => {
             // </nav>
             <>
             <nav className="navbar navbar-expand-lg bg-white header-menu">
-                <div className="container w-100">
-                    <a className="navbar-brand" href="#">
-                        <Logomarca imagem={"./images/logo.svg"}/>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className="menu-top">
+                <div className="container">
+                    <div className="menu-top w-100">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <a className="navbar-brand" href="#">
+                                <Logomarca imagem={"./images/logo.svg"}/>
+                            </a>
                             <div className="input-group">
                                 <input type="search" className="form-control border-0" placeholder="Pesquisar produtos..." aria-label="Search"/>
                                 <span className="input-group-text border-0">
@@ -68,7 +68,6 @@ const Header = () => {
                             </ul>
                         </div>
                     </div>
-               
                 </div>
             </nav>
 
