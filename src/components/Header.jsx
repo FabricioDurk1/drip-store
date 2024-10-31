666231a8357b88fe0fabd8e9b200f77186cf642b
Rafael Leite <costtaraphael@gmail.com>
2024-10-30 21:44:12 -0300

    ajustes no header responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,9 +30,9 @@ const Header = () => {
 
             //     </div>
             // </nav>
-
+            <>
             <nav className="navbar navbar-expand-lg bg-white header-menu">
-                <div className="container-fluid">
+                <div className="container w-100">
                     <a className="navbar-brand" href="#">
                         <Logomarca imagem={"./images/logo.svg"}/>
                     </a>
@@ -40,37 +40,39 @@ const Header = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className="input-group">
-                            <input type="search" className="form-control border-0" placeholder="Pesquisar produtos..." aria-label="Search"/>
-                            <span className="input-group-text border-0">
-                                <img src="./images/lupa.svg" alt="Ícone de busca" width="20px" height="20px"/>
-                            </span>
+                        <div className="menu-top">
+                            <div className="input-group">
+                                <input type="search" className="form-control border-0" placeholder="Pesquisar produtos..." aria-label="Search"/>
+                                <span className="input-group-text border-0">
+                                    <img src="./images/lupa.svg" alt="Ícone de busca" width="20px" height="20px"/>
+                                </span>
+                            </div>
+                            <ul className="navbar-nav m-0">
+                                <li className="nav-item">
+                                    <a className="cadastro" href="#">Cadastre-se</a>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="btn" type="submit">Entrar</button>
+                                </li>
+                                <li className="nav-item">
+                                    <img src="./images/cart.svg" alt=""/>
+                                </li>
+                            </ul>
                         </div>
-                        <ul className="navbar-nav m-0">
-                            <li className="nav-item">
-                                <a className="cadastro" href="#">Cadastre-se</a>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn" type="submit">Entrar</button>
-                            </li>
-                            <li className="nav-item">
-                                <img src="./images/cart.svg" alt=""/>
-                            </li>
-                        </ul>
+                        <div className="menu-bottom">
+                            <ul className="menu-options navbar-nav">
+                                <li className="nav-item"><a href="">Produtos</a></li>
+                                <li className="nav-item"><a href="">Categorias</a></li>
+                                <li className="nav-item"><a href="">Home</a></li>
+                                <li className="nav-item"><a href="">Meus pedidos</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="menu-options navbar-nav">
-                            <li className="nav-item"><a href="">Produtos</a></li>
-                            <li className="nav-item"><a href="">Categorias</a></li>
-                            <li className="nav-item"><a href="">Home</a></li>
-                            <li className="nav-item"><a href="">Meus pedidos</a></li>
-                        </ul>
-                    </div>
+               
                 </div>
             </nav>
-            
 
-
+         </>
      );
 }
  
