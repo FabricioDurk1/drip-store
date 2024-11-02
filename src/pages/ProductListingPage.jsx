bd7a5b809d570d5993feba83ee33e7391abe97ad
Fabricio Amorim <fabriciofkz61@gmail.com>
2024-11-02 20:16:09 -0300

    Initial commit with filter and product listing page

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -1,4 +1,5 @@
 import ProductList from '../components/ProductList';
+import FilterGroup from '../components/FilterGroup'; // Importe o componente
 import productData from "../assets/productlist.json";
 
 const ProductListingPage = () => {
@@ -7,10 +8,55 @@ const ProductListingPage = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-3">
-                        <h1>O filtro aqui</h1>
+                        <h2>Filtrar por</h2>
+                        <hr />
+
+                        {/* Filtros */}
+                        <FilterGroup
+                            title="Marca"
+                            inputType="checkbox"
+                            options={[
+                                { text: "Adidas" },
+                                { text: "Calenciaga" },
+                                { text: "K-Swiss" },
+                                { text: "Nike" },
+                                { text: "Puma" }
+                            ]}
+                        />
+
+                        <FilterGroup
+                            title="Categoria"
+                            inputType="checkbox"
+                            options={[
+                                { text: "Esporte e lazer" },
+                                { text: "Casual" },
+                                { text: "Utilitário" },
+                                { text: "Corrida" }
+                            ]}
+                        />
+
+                        <FilterGroup
+                            title="Gênero"
+                            inputType="checkbox"
+                            options={[
+                                { text: "Masculino" },
+                                { text: "Feminino" },
+                                { text: "Unissex" }
+                            ]}
+                        />
+
+                        <FilterGroup
+                            title="Estado"
+                            inputType="radio"
+                            options={[
+                                { text: "Novo" },
+                                { text: "Usado" }
+                            ]}
+                        />
                     </div>
+
                     <div className="col-9">
-                    <ProductList products={productData} />
+                        <ProductList products={productData} />
                     </div>
                 </div>
             </div>
